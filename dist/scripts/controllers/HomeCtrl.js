c23f6e1f044989a6a637b618695d6760cb3c2878
Jim Rohrer <jim.rohrer@gmail.com>
2016-09-22 11:43:25 -0700

    open modal

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,6 +1,7 @@
 (function() {
-    function HomeCtrl(Fixtures, $firebaseArray) {  // removed "Fixtures, "
+    function HomeCtrl(Fixtures, $firebaseArray, $uibModal) {  // removed "Fixtures, "
     	// this.rooms = Fixtures.getCommunity();
+    	var $ctrl = this;
 
 
 		var ref = firebase.database().ref().child("rooms");
@@ -9,14 +10,21 @@
 		this.rooms = $firebaseArray(ref);
 
 
-		this.rooms.$add({roomName: "TRMS"}); // functional test for adding a room
+//		this.rooms.$add({roomName: "TRMS"}); // functional test for adding a room
 
 
 		window.foo = this.rooms;
+
+		$ctrl.open = function () {
+			console.log("Open was clicked")
+			var modalInstance = $uibModal.open({
+			  templateUrl: 'myModalContent.html',
+			});
+		};
 
 	}
 
     angular
         .module('chit')
-        .controller('HomeCtrl', ['Fixtures', '$firebaseArray', HomeCtrl]);  // Removed "'Fixtures',"
+        .controller('HomeCtrl', ['Fixtures', '$firebaseArray', '$uibModal', HomeCtrl]);  // Removed "'Fixtures',"
 })();
